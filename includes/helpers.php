c41be87f94a70584baaf4b58447df0629dbe5bf4
anilpanthi <apanthi21@gmail.com>
2023-08-09 16:51:11 +0545

    Changed Folder Name To form-entryid/filename

--- includes/helpers.php
+++ includes/helpers.php
@@ -130,7 +130,7 @@ function s3sync_send_entry_files_to_s3( $entry, $form_id, $field_id, $keys, $unl
 		 */
 
 		// $object_path = apply_filters( 's3sync_put_object_file_path', "form-{$form_id}/{$entry['id']}/{$file_name}", $file, $file_name, $field_id, $form_id, $entry );
-		$object_path = apply_filters( 's3sync_put_object_file_path',"form-{$entry['id']}-{$folder_name}/{$file_name}", $file, $file_name, $field_id, $form_id, $entry );
+		$object_path = apply_filters( 's3sync_put_object_file_path',"form-{$entry['id']}/{$file_name}", $file, $file_name, $field_id, $form_id, $entry );
 		/**
 		 * Privacy setting for the file.
 		 * See https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl_overview.html#canned-acl for possible ACL choices
